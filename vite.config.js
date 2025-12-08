84f45c268ed98533458584fb0bc8b5a7ba268c2b
Jhonier Duran <djhoniersamir@gmail.com>
2025-12-08 08:31:36 -0500

    new config in vite

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,53 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import viteCompression from "vite-plugin-compression";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    allowedHosts: [
-      "allyn-sensitometric-dulce.ngrok-free.dev",
-    ]
-  }
-})
+  plugins: [
+    react(),
+
+    // Gzip
+    viteCompression({
+      algorithm: "gzip",
+      ext: ".gz",
+      threshold: 1024,
+    }),
+
+    // Brotli
+    viteCompression({
+      algorithm: "brotliCompress",
+      ext: ".br",
+      threshold: 1024,
+    }),
+  ],
+
+  build: {
+    target: "esnext",
+    minify: "esbuild",
+    cssMinify: true,
+    sourcemap: false,
+
+    // Eliminar console/debugger
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name]-[hash].js",
+        chunkFileNames: "assets/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash][extname]",
+
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("react")) return "react";
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
+});
